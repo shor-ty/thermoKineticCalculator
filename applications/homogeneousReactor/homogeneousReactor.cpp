e9da58641a9d21ada3e25e5ad128ffd2f8dd3a73
Tobias Holzmann <Tobias.Holzmann@Holzmann-cfd.com>
2020-04-25 23:15:43 +0200

    Cleaning application folder. Created homogeneousReactor source file

---
+++ applications/homogeneousReactor/homogeneousReactor.cpp
@@ -0,0 +1,65 @@
+/*---------------------------------------------------------------------------*\
+  c-o-o-c-o-o-o             |
+  |     |     T hermo       | Open Source Thermo-Kinetic Library
+  c-o-o-c     K iknetic     |
+  |     |     C onstructor  | Copyright (C) 2020 Holzmann CFD
+  c     c-o-o-o             |
+-------------------------------------------------------------------------------
+License
+    This file is part of Automatic Flamelet Creator.
+
+    TKC is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    TKC is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TKC; if not, see <http://www.gnu.org/licenses/>
+
+Description
+
+    Transient thermo-kinetic 0D calculator for detailed chemistry analysis.
+
+
+\*---------------------------------------------------------------------------*/
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+using namespace TKC;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char** argv)
+{
+
+    const std::clock_t startTime = clock();
+
+    Info<< Header() << endl;
+
+    //- Read the properties of the analysis
+    Properties properties;
+
+    //- Create Objects for calculation
+
+
+    //- Initialize 0D calculation
+
+
+    //- Solve chemistry and save data regarding the use input
+
+
+
+    Footer(startTime);
+
+    return 0;
+}
+
+
+// ************************************************************************* //
+
